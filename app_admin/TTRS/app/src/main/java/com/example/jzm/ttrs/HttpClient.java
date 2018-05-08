f02524c8e408e55894b7fbd00943c0896a9c7f3d
Cai Yaxing <737363395@qq.com>
2018-05-08 13:53:02 +0800

    f

---
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrs/HttpClient.java
@@ -0,0 +1,31 @@
+package com.example.jzm.ttrs;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class HttpClient {
+    private String command;
+    private String responseData;
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String run() {
+        try {
+            OkHttpClient client = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).readTimeout(20, TimeUnit.SECONDS).build();
+            RequestBody requestBody = new FormBody.Builder().add("input", command).build();
+            Request request = new Request.Builder().url("http://120.79.236.3:5000/action/post").post(requestBody).build();
+            Response response = client.newCall(request).execute();
+            responseData = response.body().string();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return responseData;
+    }
+}
---
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrs/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.jzm.ttrs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private List<Train> trainList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer =  findViewById(R.id.activity_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Toast.makeText(MainActivity.this, "登录成功~♪（＾∀＾●）", Toast.LENGTH_SHORT).show();
+
+        initializeTrains();
+        RecyclerView recyclerView = findViewById(R.id.front_page_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        TrainAdapter adapter = new TrainAdapter(trainList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initializeTrains(){
+        for (int i = 0; i < 50; i++) {
+            Train testTrain = new Train("c101", "fuck", "C", "北京", "夏威夷", "08:00", "08:01",
+                    "2018-03-28", "2018-03-28");
+            trainList.add(testTrain);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.activity_main);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_homepage) {
+            Toast.makeText(MainActivity.this, "你已经在首页了哦~w(ﾟДﾟ)w", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_train) {
+            Intent intent = new Intent(MainActivity.this, TrainQuery.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_user) {
+            Intent intent = new Intent(MainActivity.this, ModifyUserInfo.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_info) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.activity_main);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
