9fc38087096758c13c17c1765f711ff6a61cdbcb
Cai Yaxing <737363395@qq.com>
2018-05-14 15:47:56 +0800

    s

---
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrs/SelectStation.java
@@ -0,0 +1,204 @@
+package com.example.jzm.ttrs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SelectStation extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private WaveSideBar mSideBar;
+    private SortAdapter mAdapter;
+    private ClearEditText mClearEditText;
+    private LinearLayoutManager manager;
+
+    private List<SortModel> mDateList;
+    private TitleItemDecoration mDecoration;
+    private List<String> list = new ArrayList<>();
+
+    /**
+     * 根据拼音来排列RecyclerView里面的数据类
+     */
+    private PinyinComparator mComparator;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_station);
+        sendRequest();
+    }
+
+    private void initViews() {
+        mComparator = new PinyinComparator();
+
+        mSideBar = findViewById(R.id.sideBar);
+
+        //设置右侧SideBar触摸监听
+        mSideBar.setOnTouchLetterChangeListener(new WaveSideBar.OnTouchLetterChangeListener() {
+            @Override
+            public void onLetterChange(String letter) {
+                //该字母首次出现的位置
+                int position = mAdapter.getPositionForSection(letter.charAt(0));
+                if (position != -1) {
+                    manager.scrollToPositionWithOffset(position, 0);
+                }
+            }
+        });
+
+        mRecyclerView = findViewById(R.id.rv);
+        mDateList = filledData(list);
+
+        // 根据a-z进行排序源数据
+        Collections.sort(mDateList, mComparator);
+
+        //RecyclerView设置manager
+        manager = new LinearLayoutManager(this);
+        mAdapter = new SortAdapter(this, mDateList);
+        mDecoration = new TitleItemDecoration(this, mDateList);
+        mAdapter.setOnItemClickListener(new SortAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent();
+                intent.putExtra("station", mAdapter.getLocation(position));
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                manager.setOrientation(LinearLayoutManager.VERTICAL);
+                mRecyclerView.setLayoutManager(manager);
+                mRecyclerView.setAdapter(mAdapter);
+                //如果add两个，那么按照先后顺序，依次渲染。
+                mRecyclerView.addItemDecoration(mDecoration);
+                mRecyclerView.addItemDecoration(new DividerItemDecoration(SelectStation.this, DividerItemDecoration.VERTICAL));
+
+
+                mClearEditText = (ClearEditText) findViewById(R.id.filter_edit);
+
+                //根据输入框输入值的改变来过滤搜索
+                mClearEditText.addTextChangedListener(new TextWatcher() {
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        //当输入框里面的值为空，更新为原来的列表，否则为过滤数据列表
+                        filterData(s.toString());
+                    }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count,
+                                                  int after) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                    }
+                });
+            }
+        });
+    }
+
+
+    /**
+     * 为RecyclerView填充数据
+     *
+     * @param date
+     * @return
+     */
+    private List<SortModel> filledData(List<String> date) {
+        List<SortModel> mSortList = new ArrayList<>();
+
+        for (int i = 0; i < date.size(); i++) {
+            SortModel sortModel = new SortModel();
+            sortModel.setName(date.get(i));
+            //汉字转换成拼音
+            String pinyin = PinyinUtils.getPingYin(date.get(i));
+            String sortString = pinyin.substring(0, 1).toUpperCase();
+
+            // 正则表达式，判断首字母是否是英文字母
+            if (sortString.matches("[A-Z]")) {
+                sortModel.setLetters(sortString.toUpperCase());
+            } else {
+                sortModel.setLetters("#");
+            }
+
+            mSortList.add(sortModel);
+        }
+        return mSortList;
+
+    }
+
+    /**
+     * 根据输入框中的值来过滤数据并更新RecyclerView
+     *
+     * @param filterStr
+     */
+    private void filterData(String filterStr) {
+        List<SortModel> filterDateList = new ArrayList<>();
+
+        if (TextUtils.isEmpty(filterStr)) {
+            filterDateList = filledData(list);
+        } else {
+            filterDateList.clear();
+            for (SortModel sortModel : mDateList) {
+                String name = sortModel.getName();
+                if (name.indexOf(filterStr.toString()) != -1 ||
+                        PinyinUtils.getFirstSpell(name).startsWith(filterStr.toString())
+                        //不区分大小写
+                        || PinyinUtils.getFirstSpell(name).toLowerCase().startsWith(filterStr.toString())
+                        || PinyinUtils.getFirstSpell(name).toUpperCase().startsWith(filterStr.toString())
+                        ) {
+                    filterDateList.add(sortModel);
+                }
+            }
+        }
+
+        // 根据a-z进行排序
+        Collections.sort(filterDateList, mComparator);
+        mDateList.clear();
+        mDateList.addAll(filterDateList);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void sendRequest(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClient client = new HttpClient();
+                    JSONObjectStringCreate jsonobjcetcreate = new JSONObjectStringCreate();
+                    jsonobjcetcreate.addStringPair("type", "list_station");
+                    client.setCommand(jsonobjcetcreate.getResult());
+                    JSONObject jsonObject = new JSONObject(client.run());
+                    String success = jsonObject.getString("success");
+                    if (success.equals("true")) {
+                        JSONArray jsonArray = jsonObject.getJSONArray("station");
+                        for (int i = 0; i < jsonArray.length(); ++i)
+                            list.add(jsonArray.getString(i));
+
+                        initViews();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
