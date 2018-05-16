4be72285a15ef95c003bbff054ad8ae3804bed41
HouBohan <spectrometer@sjtu.edu.cn>
2018-05-16 20:05:50 +0800

    fuck you

--- app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/TrainAddDetail.java
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/TrainAddDetail.java
@@ -22,6 +22,8 @@ public class TrainAddDetail extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 
     @Override
--- app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/TrainQuery.java
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/TrainQuery.java
@@ -1,4 +1,4 @@
-package com.example.jzm.ttrsadmin;
+ package com.example.jzm.ttrsadmin;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
