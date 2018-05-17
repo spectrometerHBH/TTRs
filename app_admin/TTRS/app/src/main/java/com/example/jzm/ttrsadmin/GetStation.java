9ca56ec86a573875612a17c3658e06c44185b467
Cai Yaxing <737363395@qq.com>
2018-05-17 21:31:47 +0800

    new

--- app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/GetStation.java
+++ app_admin/TTRS/app/src/main/java/com/example/jzm/ttrsadmin/GetStation.java
@@ -1,5 +1,8 @@
 package com.example.jzm.ttrsadmin;
 
+import android.animation.TimeAnimator;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -10,23 +13,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-import io.netty.handler.codec.http.HttpObject;
-
-public class GetStaion extends AppCompatActivity {
+public class GetStation extends AppCompatActivity {
     private LinearLayout linearLayout;
     private Button addButton;
     private Button confirmButton;
     private Context context;
     private String trainId;
     private String trainName;
+    private String trainCatalog;
     private ArrayList<String> seatTypes = new ArrayList<>();
     private List<View> viewList = new ArrayList<>();
     private List<List<View>> subviewList = new ArrayList<>();
@@ -36,11 +40,11 @@ public class GetStaion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_staion);
         initializeWidgets();
-
         Intent intent = getIntent();
         trainId = intent.getStringExtra("trainId");
         trainName = intent.getStringExtra("trainName");
         seatTypes = intent.getStringArrayListExtra("seats");
+        trainCatalog = intent.getStringExtra("trainCatalog");
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +56,59 @@ public class GetStaion extends AppCompatActivity {
                 newView.setLayoutParams(layoutParams);
                 linearLayout.addView(newView);
                 viewList.add(newView);
+
+                final TextView arrive = viewList.get(viewList.size() - 1).findViewById(R.id.arrive_time_item);
+                arrive.setClickable(true);
+                arrive.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final Calendar c = Calendar.getInstance();
+                        final int hour = c.get(Calendar.HOUR_OF_DAY);
+                        final int minute = c.get(Calendar.MINUTE);
+                        new TimePickerDialog(GetStation.this, new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                arrive.setText((hourOfDay < 10 ? "0" : "")+ hourOfDay + ":" +(minute < 10 ? "0" : "") + minute);
+                            }
+                        }, hour, minute, true).show();
+                    }
+                });
+                final TextView depart = viewList.get(viewList.size() - 1).findViewById(R.id.depart_time_item);
+                depart.setClickable(true);
+                depart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final Calendar c = Calendar.getInstance();
+                        final int hour = c.get(Calendar.HOUR_OF_DAY);
+                        final int minute = c.get(Calendar.MINUTE);
+                        new TimePickerDialog(GetStation.this, new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                depart.setText((hourOfDay < 10 ? "0" : "") + hourOfDay + ":" +(minute < 10 ? "0" : "") + minute);
+                            }
+                        }, hour, minute, true).show();
+                    }
+                });
+                final TextView stopover = viewList.get(viewList.size() - 1).findViewById(R.id.stopover_item);
+                stopover.setClickable(true);
+                stopover.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final Calendar c = Calendar.getInstance();
+                        final int hour = c.get(Calendar.HOUR_OF_DAY);
+                        final int minute = c.get(Calendar.MINUTE);
+                        new TimePickerDialog(GetStation.this, new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                stopover.setText((hourOfDay < 10 ? "0" : "") + hourOfDay + ":" +(minute < 10 ? "0" : "") + minute);
+                            }
+                        }, hour, minute, true).show();
+                    }
+                });
+
             }
         });
 
@@ -75,7 +132,7 @@ public class GetStaion extends AppCompatActivity {
                     jsonArrayStringCreate.addJSONObject(getJSON(viewList.get(i), i));
                 }
                 commandMaker.addJSONArrayPair("station", jsonArrayStringCreate.getResult());
-                commandMaker.addStringPair("catalog", "G");
+                commandMaker.addStringPair("catalog", trainCatalog);
                 sendRequest(commandMaker.getResult());
             }
         });
@@ -84,12 +141,14 @@ public class GetStaion extends AppCompatActivity {
     private String getJSON(View view, int parentPos){
         JSONObjectStringCreate jsonObjectStringCreate = new JSONObjectStringCreate();
         EditText stationEditText = view.findViewById(R.id.station_item);
-        EditText arriveTimeEditText = view.findViewById(R.id.arrive_time_item);
-        EditText startTimeEditText = view.findViewById(R.id.depart_time_item);
-        EditText stopoverTimeEditText = view.findViewById(R.id.stopover_item);
+        TextView arriveTimeEditText = view.findViewById(R.id.arrive_time_item);
+        TextView startTimeEditText = view.findViewById(R.id.depart_time_item);
+        TextView stopoverTimeEditText = view.findViewById(R.id.stopover_item);
         jsonObjectStringCreate.addStringPair("name", stationEditText.getText().toString());
-        jsonObjectStringCreate.addStringPair("timearriv", arriveTimeEditText.getText().toString());
-        jsonObjectStringCreate.addStringPair("timestart", startTimeEditText.getText().toString());
+        if (view == viewList.get(0)) jsonObjectStringCreate.addStringPair("timearriv", "xx:xx");
+        else jsonObjectStringCreate.addStringPair("timearriv", arriveTimeEditText.getText().toString());
+        if (view == viewList.get(viewList.size() - 1)) jsonObjectStringCreate.addStringPair("timestart", "xx:xx");
+        else jsonObjectStringCreate.addStringPair("timestart", startTimeEditText.getText().toString());
         jsonObjectStringCreate.addStringPair("timestopover", stopoverTimeEditText.getText().toString());
         JSONArrayStringCreate jsonArrayStringCreate = new JSONArrayStringCreate();
         List<View> templist = subviewList.get(parentPos);
@@ -129,7 +188,7 @@ public class GetStaion extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(GetStaion.this, message, Toast.LENGTH_SHORT).show();
+                Toast.makeText(GetStation.this, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
